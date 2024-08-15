188c4ef8f2c06b2463c43f226670eee5a8d52253
suvorovjr <pglavan1998@mail.ru>
2024-08-15 15:01:20 +0300

    add changes in project settings

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,14 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from loguru import logger
 
 BASE_DIR = Path(__file__).parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+
+LOG_FILE_PATH = BASE_DIR / 'logs' / 'async_parser.log'
+LOG_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+logger.add(LOG_FILE_PATH, rotation="10 MB", retention="10 days", mode="a")
