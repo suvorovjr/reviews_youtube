af3600bdd58f3bedc13b20d4f20576b38a4096c4
suvorovjr <pglavan1998@mail.ru>
2024-08-15 10:02:34 +0300

    add google_api_client.py in project

---
+++ src/google_api_client.py
@@ -0,0 +1,17 @@
+from config import settings
+
+
+class GoogleAPIClient:
+    api_key = settings.YOUTUBE_API_KEY
+
+    def __init__(self):
+        pass
+
+    def get_chanel_id(self):
+        pass
+
+    async def get_all_videos(self):
+        pass
+
+    async def get_reviews(self):
+        pass
